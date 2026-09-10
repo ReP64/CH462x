1876f3176cc2171809595b62d09005526dbdb1f4
ReP64 <rene@lre.nl>
2026-09-10 08:54:50 +0200

    Format code for better readability in Indicators.ino

--- examples/Indicators/Indicators.ino
+++ examples/Indicators/Indicators.ino
@@ -1,4 +1,18 @@
 #include <CH462x.h>
-CH462x lcd(21,20,18,19);
-void setup(){lcd.begin();}
-void loop(){lcd.setKg();delay(1000);lcd.clearIndicators();lcd.setNet();delay(1000);lcd.clearIndicators();}
+
+CH462x lcd(21,20,18,19); // bl , cs, clk data
+
+void setup()
+{
+  lcd.begin();
+}
+
+void loop()
+{ 
+  lcd.setKg();  
+  delay(1000);
+  lcd.clearIndicators();
+  lcd.setNet();
+  delay(1000);
+  lcd.clearIndicators();
+}
