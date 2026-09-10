8250831bb12976857057c40b6470d7c189322c9b
ReP64 <rene@lre.nl>
2026-09-10 08:55:51 +0200

    Format code for better readability in BasicDemo.ino

--- examples/BasicDemo/BasicDemo.ino
+++ examples/BasicDemo/BasicDemo.ino
@@ -1,4 +1,17 @@
 #include <CH462x.h>
-CH462x lcd(21,20,18,19);
-void setup(){lcd.begin();lcd.blOn();}
-void loop(){lcd.show("1234.56");delay(2000);lcd.show("PASS");delay(2000);} 
+
+CH462x lcd(21,20,18,19); // bl , cs, clk data
+
+void setup()
+{
+  lcd.begin();
+  lcd.blOn();
+}
+
+void loop()
+{
+  lcd.show("1234.56");
+  delay(2000);
+  lcd.show("PASS");
+  delay(2000);
+} 
