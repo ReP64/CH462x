3b7cc67dd49a8d4074e6c43185e43f6ceba84d3d
ReP64 <rene@lre.nl>
2026-09-10 08:56:40 +0200

    Refactor setup and loop functions for clarity

--- examples/AllSegments/AllSegments.ino
+++ examples/AllSegments/AllSegments.ino
@@ -1,4 +1,16 @@
 #include <CH462x.h>
-CH462x lcd(21,20,18,19);
-void setup(){lcd.begin();}
-void loop(){lcd.allOn();delay(3000);lcd.clear();delay(1000);} 
+
+CH462x lcd(21,20,18,19); // bl , cs, clk data
+
+void setup()
+{
+  lcd.begin();
+}
+
+void loop()
+{
+  lcd.allOn();
+  delay(3000);
+  lcd.clear();
+  delay(1000);
+} 
